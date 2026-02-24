f97b9954772f99ffc575bdde7898e293e5154551
Chris Kangaji <christian.kangaji@live.se>
2026-02-24 15:56:35 +0100

    fixed typescript type issue by creating an addition to barrel index file

---
+++ src/components/index.d.ts
@@ -0,0 +1,11 @@
+import type { ComponentType } from "react";
+
+export const About: ComponentType<any>;
+export const Contact: ComponentType<any>;
+export const Experience: ComponentType<any>;
+export const Feedbacks: ComponentType<any>;
+export const Hero: ComponentType<any>;
+export const Navbar: ComponentType<any>;
+export const Tech: ComponentType<any>;
+export const Works: ComponentType<any>;
+export const StarsCanvas: ComponentType<any>;
