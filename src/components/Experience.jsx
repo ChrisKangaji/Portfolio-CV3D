ff1477a9685b9ea197944a0803b6ab79b0cfe0a5
Chris Kangaji <christian.kangaji@live.se>
2026-04-22 18:38:35 +0200

    started working with experience section. Did some configurations of packages in order to get react-vertical-timeline-component to work

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import { VerticvalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
+
+import 'react-vertical-timeline-component/style.min.css';
+
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+import { sectionWrapper } from '../hoc';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}
+        >What I have done so far</p>
+        <h2 className={`${styles.sectionHeadText} mt-4`} //Having issues with appliting margin to the heading, so added it here instead of the styles file
+        >Work Experience.</h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        {/* Loop through the experiences array and create a VerticalTimelineElement for each experience */}
+        {experiences.map((experience, index) => (
+            <VerticalTimelineElement
+              contentStyle={{ background: '#1d1836', color: '#fff' }}
+              key={index}
+              contentArrowStyle={{ borderRight: '7px solid #232631' }}
+              date={experience.date}
+              iconStyle={{ background: experience.iconBg }}
+              icon={
+                <div className='flex justify-center items-center w-full h-full'>
+                  <img src={experience.icon} alt={experience.company_name} className='w-[60%] h-[60%] object-contain' />
+                </div>
+              }
+            >
+              <div>
+                <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+                <p className='text-secondary text-[16px] font-semibold' style={{ margin: 0 }}>{experience.company_name}</p>
+              </div>
+              <ul className='mt-5 list-disc ml-5 space-y-2'>
+                {experience.points.map((point, index) => (
+                  <li key={index} className='text-white-100 text-[14px] pl-1 tracking-wider'>{point}</li>
+                ))}
+              </ul>
+            </VerticalTimelineElement>
+          ))}
+      </div>
+
+    </>
   )
 }
 
-export default Experience
+export default sectionWrapper(Experience, "work");
