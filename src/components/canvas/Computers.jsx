121c97c20e8881c022c12a1a0ea7f0dc9554316b
Chris Kangaji <christian.kangaji@live.se>
2026-04-22 11:23:24 +0200

    worked on overview section, components and header

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,17 +4,18 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   // const computer = useGLTF('./desktop_pc/scene.gltf');
-  const computer = useGLTF('./planet/scene.gltf');
+  const planet = useGLTF('./planet/scene.gltf');
 
   return (
     <mesh>
       {/* Starting with a mesh so that we can create our 3d model */}
       <hemisphereLight intensity={2.15} groundColor="black" />
       <pointLight intensity={2} />
+      //Do some more shanges to the spotlight at some point
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-10, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={10}
@@ -22,11 +23,18 @@ const Computers = () => {
         shadow-mapSize={1024}
       />
       <primitive
-        object={computer.scene}
-        scale={0.75}
-        // position={[0, -2.5, -1.5]}
-        // rotation={[-0.001, -0.2, -0.045]}
-        position={[0, -3.5, -1.5]}
+        // Computer model
+        // object={computer.scene}
+        // scale={0.75}
+        // // position={[0, -2.5, -1.5]}
+        // // rotation={[-0.001, -0.2, -0.045]}
+        // position={[0, -3.5, -1.5]}
+        // rotation={[-0.01, -0.2, -0.1]}
+
+        //Planet model
+        object={planet.scene}
+        scale={isMobile ? 1.5 : 2}
+        position={isMobile ? [0, -0.25, -1.5] : [0, -0.75, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
 
@@ -36,6 +44,23 @@ const Computers = () => {
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    // Remove the listener when the component is unmounted    
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <Canvas
@@ -50,7 +75,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
