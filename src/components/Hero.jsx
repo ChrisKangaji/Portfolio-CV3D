b081bfcf096f8cbdb5cebb227aafc4c98da0b2b0
Chris Kangaji <christian.kangaji@live.se>
2026-05-17 17:28:31 +0200

    Worked on stars canvas. Minor refactoring

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputerCanvas, EarthCanvas } from './canvas';
 import "./home.css";
 
 const Hero = () => {
-  //List of words to display
+    //List of words to display
     const toRotate = [", Software Developer", ", Fullstack Developer"];
 
     //Loop number, set to 0, will be used to loop through the words based on the index
@@ -76,11 +76,17 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        
+        {/* Decorative line elements */}
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+
+        {/* Background image */}
         <div className="home__img"></div>
+
+        {/* Typing effect and text */}
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Chris
                 {/* <svg
@@ -147,6 +153,7 @@ const Hero = () => {
       {/* <ComputerCanvas /> */}
       <EarthCanvas />
       
+      {/* Scroll down indicator */}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2"> 
